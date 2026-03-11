44779150f34c88def4e4b16c125e478bf1eb0f8a
Angel Barrera <st5043@utr.edu.mx>
2026-03-11 15:08:46 -0600

    Added code comments

--- exercise1.js
+++ exercise1.js
@@ -1,11 +1,11 @@
 // Angel Gabriel Barrera Rangel ST: 5043
 // Exercise 2
 // Create a function to print a staircase
-function staircase(n){
-    for (let i = 0 ; i < n; i++){
-        print = "#".repeat(i+1);
-        console.log(print);
+function staircase(n){ // It gets the number of steps as a parameter
+    for (let i = 0 ; i < n; i++){ // Loop to print the steps
+        print = "#".repeat(i+1); // It creates a string with the number of "#" equal to the current step
+        console.log(print); 
     }
 }
 
-staircase(2);
+staircase(2); // it calls the function with 2 steps
